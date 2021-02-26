8e9e6837a9d4e9b20ad6cbef5ec326301751842d
Igor Dalepiane da Costa <igordalepiane@gmail.com>
2021-02-26 11:49:27 -0300

    Day 3: Jest, Surveys, Code refactoring

---
+++ src/database/migrations/1614345348810-CreateSurveys.ts
@@ -0,0 +1,33 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateSurveys1614345348810 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "surveys",
+                columns: [
+                    {
+                        name: "id",
+                        type: "uuid",
+                        isPrimary: true,
+                    },
+                    {
+                        name: "title",
+                        type: "varchar",
+                    },
+                    {
+                        name: "description",
+                        type: "varchar",
+                    },
+                    {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "now()",
+                    },
+                ],
+            })
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {}
+}
